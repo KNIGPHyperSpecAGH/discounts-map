ea069f1c32fc88e31ca5dea1b5416877bfe1250f
Karol Bała <karol.bala@interia.pl>
2026-02-26 18:28:09 +0100

    Task 16 separate marker icons

--- src/features/map/DiscountMarkers.tsx
+++ src/features/map/DiscountMarkers.tsx
@@ -5,9 +5,19 @@ interface DiscountMarkersProps {
   map: maplibregl.Map | null;
   discounts: Array<{
     coordinates: [number, number];
+    category: string;
   }>;
-  onMarkerClick: () => void;
+  onMarkerClick: (discount: any) => void;
 }
+
+const ICON_MAP: Record<string, string> = {
+  //Zrobione troche losowo, można pomyśleć żeby jakoś lepiej te kategorie dopasować
+  Gastronomia: 'url("/icons/znacznik_gastronomia.svg")',
+  Inne: 'url("/icons/znacznik_inne.svg")',
+  Rozrywka: 'url("/icons/znacznik_kultura_sztuka.svg")',
+  Sport: 'url("/icons/znacznik_zdrowie_sport.svg")',
+  default: 'url("/icons/znacznik_inne.svg")',
+};
 
 export default function DiscountMarkers({
   map,
@@ -26,12 +36,21 @@ export default function DiscountMarkers({
     }> = [];
 
     discounts.forEach((discount) => {
-      const marker = new maplibregl.Marker()
+      const dom_element = document.createElement("div");
+      dom_element.className =
+        "w-10 h-10 cursor-pointer bg-contain bg-no-repeat bg-center";
+      const icon = ICON_MAP[discount.category] || ICON_MAP.default;
+      dom_element.style.backgroundImage = icon;
+      console.log(icon);
+      const marker = new maplibregl.Marker({
+        element: dom_element,
+        anchor: "bottom",
+      })
         .setLngLat(discount.coordinates)
         .addTo(map);
 
       const markerElement = marker.getElement();
-      const handleClick = () => onMarkerClick();
+      const handleClick = () => onMarkerClick(discount);
       markerElement.addEventListener("click", handleClick);
       markerListeners.push({ element: markerElement, handler: handleClick });
 
